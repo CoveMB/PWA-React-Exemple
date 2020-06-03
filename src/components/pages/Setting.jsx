0e2f25344e7bc884ca33fcab3538da5b796f7f49
Benjamin Marquis <bmarquiscom@gmail.com>
2020-06-03 08:54:06 -0400

    Update notification

--- src/components/pages/Setting.jsx
+++ src/components/pages/Setting.jsx
@@ -12,51 +12,6 @@ const Setting = () => {
 
   const [ enableNotification, setEnableNotification ] = useState(false);
 
-  useEffect(() => {
-
-    // (async () => {
-
-    //   const notification = 'Notification enabled';
-    //   const notificationOptions = {
-    //     body    : 'You can turn them off in your settings',
-    //     badge   : '/images/icons/app-icon-72x72.png',
-    //     icon    : '/images/icons/app-icon-72x72.png',
-    //     renotify: true,
-    //     tag     : 'confirm-notification',
-    //     vibrate : [
-    //       100,
-    //       50,
-    //       20
-    //     ],
-    //     actions: [
-    //       {
-    //         action: 'confirm', title: 'Noted!'
-    //       },
-    //       {
-    //         action: 'confused', title: 'What?'
-    //       }
-    //     ]
-    //   };
-
-    //   if ('serviceWorker' in navigator) {
-
-
-    //     // If the service worker is available in the the browser use it to show notification
-    //     const sw = await navigator.serviceWorker.ready;
-
-    //     sw.showNotification(notification, notificationOptions);
-
-    //   } else {
-
-    //     // Else use regular notification API
-    //     new Notification(notification, notificationOptions);
-
-    //   }
-
-    // })();
-
-  }, []);
-
   const toggleNotification = async () => {
 
     if (Notification.permission !== 'granted') {
--- src/service-worker/notificationHelper.js
+++ src/service-worker/notificationHelper.js
@@ -15,9 +15,31 @@ const handleNotification = async (event) => {
 
   return setTimeout(() => {
 
-    self.registration.showNotification('Hello again from service worker working in the background');
+    const notification = 'Hello again from service worker!';
+    const notificationOptions = {
+      body    : 'This event happened in the background',
+      badge   : '/images/icons/app-icon-72x72.png',
+      icon    : '/images/icons/app-icon-72x72.png',
+      renotify: true,
+      tag     : 'confirm-notification',
+      vibrate : [
+        100,
+        50,
+        20
+      ],
+      actions: [
+        {
+          action: 'confirm', title: 'So cool'
+        },
+        {
+          action: 'confused', title: 'Stop now'
+        }
+      ]
+    };
 
-  }, 40000);
+    self.registration.showNotification(notification, notificationOptions);
+
+  }, 400000);
 
 };
 
